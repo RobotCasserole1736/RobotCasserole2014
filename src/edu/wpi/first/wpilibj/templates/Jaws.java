6969fc0cad6c55f0b3a1c6d8527b82f564db28ec
Aaron <>
2014-01-14 20:37:41 -0600

    - Worked on Jaws class (check class for details)

--- src/edu/wpi/first/wpilibj/templates/Jaws.java
+++ src/edu/wpi/first/wpilibj/templates/Jaws.java
@@ -5,11 +5,19 @@
 
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+
 public class Jaws {
+    Solenoid bottomJawLeftSolenoid, bottomJawRightSolenoid, topJawSolenoid;
+    Talon rollerTalon;
     
-    public Jaws(int bottomJawLeftSolenoidId, int bottomJawRightSolenoidId, int topJawSolenoidId, int rollerSpikeId)
+    public Jaws(int bottomJawLeftSolenoidId, int bottomJawRightSolenoidId, int topJawSolenoidId, int rollerTalonId)
     {
+        bottomJawLeftSolenoid = new Solenoid(bottomJawLeftSolenoidId);
+        bottomJawRightSolenoid = new Solenoid(bottomJawRightSolenoidId);
+        topJawSolenoid = new Solenoid(topJawSolenoidId);
+        rollerTalon = new Talon(rollerTalonId);
         
     }
-            
 }
