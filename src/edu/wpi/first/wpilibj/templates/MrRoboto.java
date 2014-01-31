32f32d4327fec7f8c8e32d881f2a0d98310b2c04
Aaron <>
2014-01-30 20:11:47 -0600

    -Finished Jaws class

--- src/edu/wpi/first/wpilibj/templates/MrRoboto.java
+++ src/edu/wpi/first/wpilibj/templates/MrRoboto.java
@@ -13,6 +13,8 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -46,6 +48,11 @@ public class MrRoboto extends IterativeRobot {
     public final int topJawSolenoidId = 0;
     public final int rollerTalonId = 0;
     
+    //Autonomous values
+    public static double startTime = -1;
+    public static int delay = 0;
+    public static int secondDelay = 0;
+    
     // Variable/Object declarations go here
     
     // Drivetrain Talon motors + RobotDrive object
@@ -64,8 +71,12 @@ public class MrRoboto extends IterativeRobot {
     Encoder leftEncoder;
     Encoder rightEncoder;
     
+    //Joystick
+    Joystick mainJoy;
+    Joystick shooterJoy;
+    
     // Transmission
-    Solenoid xmissionSol1, xmissionSol2;
+    Solenoid xmissionSol1;
     Compressor xmissionCompressor;
     
     
@@ -89,24 +100,65 @@ public class MrRoboto extends IterativeRobot {
         
         // Construct transmission
         this.xmissionSol1 = new Solenoid(XMISSION_SOL1_ID);
-        this.xmissionSol2 = new Solenoid(XMISSION_SOL2_ID);
         this.xmissionCompressor = new Compressor(PRESSURE_SW_ID, COMPRESSOR_RELAY_ID);
         
+        //Construct joystick
+        mainJoy = new Joystick(1);
+        shooterJoy = new Joystick(2);
         
+        jaw = new Jaws(bottomJawLeftSolenoidId, bottomJawRightSolenoidId, topJawSolenoidId, rollerTalonId, topJawSolenoidId, topJawSolenoidId, shooterJoy);
+        
+        xmissionCompressor.start();
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+        autoDriveThenShoot();
+    }
+    
+    public void autoDriveThenShoot() {
+        if(startTime == -1) {
+            startTime = Timer.getFPGATimestamp();
+        }
+        if(startTime + delay < Timer.getFPGATimestamp()) {
+            driveTrain.drive(1, 0);
+        }
+        else if(startTime + delay > Timer.getFPGATimestamp() && startTime + secondDelay < Timer.getFPGATimestamp())
+        {
+            driveTrain.drive(0, 0);
+            jaw.robotShoot();
+        }
+        else
+        {
+            jaw.shooterReset();
+        }
+    }
+    
+    public void autoShootThenDrive() {
+        if(startTime == -1) {
+            startTime = Timer.getFPGATimestamp();
+            jaw.robotShoot();
+        }
+        if(startTime + delay < Timer.getFPGATimestamp())
+        {
+            jaw.shooterReset();
+            driveTrain.drive(1, 0);
+        }
+        else
+        {
+            driveTrain.drive(0, 0);
+        }
     }
 
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() {
-        
+    public void teleopPeriodic() 
+    {
+        driveTrain.arcadeDrive(mainJoy);
+        jaw.update();
     }
     
     /**
